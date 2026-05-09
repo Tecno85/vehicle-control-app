aa65bfbcbb8e4ca2fb7b0a8422a77e4d8c85639a
Ivan Dario Madrid Daza <tecno85@gmail.com>
2026-05-09 07:47:52 -0500

    feat: mejorar formulario de gasto

--- app/src/main/java/com/ivanmadrid/vehiclecontrolapp/presentation/screens/expenses/ExpenseFormScreen.kt
+++ app/src/main/java/com/ivanmadrid/vehiclecontrolapp/presentation/screens/expenses/ExpenseFormScreen.kt
@@ -1,13 +1,14 @@
 package com.ivanmadrid.vehiclecontrolapp.presentation.screens.expenses
 
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
@@ -19,6 +20,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.ivanmadrid.vehiclecontrolapp.domain.model.Vehicle
 
@@ -47,6 +49,7 @@ fun ExpenseFormScreen(
     Column(
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(16.dp)
     ) {
         Button(
